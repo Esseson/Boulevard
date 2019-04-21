834b4c1052b7448cdadbab00c5dfe5f4c31961ca
Esseson <Essesonahmed@yahoo.com>
2019-04-21 14:10:12 -0700

    Landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -62,60 +62,79 @@ const Index = () => (
     <Parallax pages={5}>
       <Hero offset={0}>
         <BigTitle>
-          Hello, <br /> I'm John Doe.
+          Hello, <br />
         </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle> <h1> Hand over the keys, get paid immediately. </h1> </Subtitle>
       </Hero>
       <Projects offset={1}>
-        <Title>Projects</Title>
+        <Title></Title>
         <ProjectsWrapper>
           <ProjectCard
-            title="Freiheit"
-            link="https://www.behance.net/gallery/58937147/Freiheit"
-            bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
+            title="Guarantee monthly payment, regardless of vacancy"
+            link=""
+            bg="linear-gradient(to right, #000000 0%, #000000 100%)"
           >
-            This project is my entry to Adobe's #ChallengeYourPerspective contest.
+
           </ProjectCard>
           <ProjectCard
-            title="Harry Potter"
-            link="https://www.behance.net/gallery/52915793/Harry-Potter"
-            bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
+            title="Tenant screening and placement completely managed by Boulevard"
+            link=""
+            bg="linear-gradient(to right, #000000 0%, #000000 100%)"
           >
-            I entered the DOCMA 2017 award with this Harry Potter inspired image.
+
           </ProjectCard>
           <ProjectCard
-            title="Tomb Raider"
-            link="https://www.behance.net/gallery/43907099/Tomb-Raider"
-            bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
+            title="Regular property inspections"
+            link=""
+            bg="linear-gradient(to right, #000000 0%, #000000 100%)"
           >
-            Recreation of a Tomb Raider Wallpaper (Fan Art)
+
           </ProjectCard>
           <ProjectCard
-            title="Eagle"
-            link="https://www.behance.net/gallery/38068151/Eagle"
-            bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
+            title="Maintenance costs covered up to $1000"
+            link=""
+            bg="linear-gradient(to right, #000000 0%, #000000 100%)"
           >
-            A fantasy image manipulation relocating the habitat of wild animals.
+          
           </ProjectCard>
+
+          <ProjectCard
+            title="Eviction management & costs covered up to $500"
+            link=""
+            bg="linear-gradient(to right, #000000 0%, #000000 100%)"
+          >
+          
+          </ProjectCard>
+
+          
+          <ProjectCard
+            title="24/7/365 tenant support"
+            link=""
+            bg="linear-gradient(to right, #000000 0%, #000000 100%)"
+          >
+          
+          </ProjectCard>
+
+          <ProjectCard
+            title="No hidden costs, no vacancy charges"
+            link=""
+            bg="linear-gradient(to right, #000000 0%, #000000 100%)"
+          >
+          
+          </ProjectCard>
+
         </ProjectsWrapper>
       </Projects>
-      <About offset={3}>
-        <Title>About</Title>
-      </About>
+
       <Contact offset={4}>
         <Inner>
           <Title>Get in touch</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+            Say <a href="">Hi</a> or find us on other platforms:{' '}
+
           </ContactText>
         </Inner>
-        <Footer>
-          &copy; 2019 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>. Made by{' '}
-          <a href="https://www.lekoarts.de">LekoArts</a>.
-        </Footer>
+
       </Contact>
     </Parallax>
   </>
